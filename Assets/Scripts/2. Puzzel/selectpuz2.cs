c9484d47595c7f50aee0b514d92f8604e8984ada
Lishani <lishani90@gmail.com>
2014-05-29 11:18:45 +0530

    puzzle added

---
+++ Assets/Scripts/2. Puzzel/selectpuz2.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class selectpuz2 : MonoBehaviour {
+	Animator anim;
+	void Start () 
+	{
+		anim = GetComponent<Animator> ();
+	}
+	void FixedUpdate () 
+	{
+		if (Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Began ) 
+		{
+			if(anim.GetBool("IsSelected")==false){
+				anim.SetBool ("IsSelected",true);
+			}
+			else{
+				anim.SetBool ("IsSelected",false);
+			}
+		}
+	}
+}
